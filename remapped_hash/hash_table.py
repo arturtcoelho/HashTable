f840ae1135f2d3e3553c276e9bcd9f0dddc3d16a
Artur Coelho <artur.temporal@hotmail.com>
2021-01-27 00:29:44 -0300

    add: hash com remapping feito

--- remapped_hash/hash_table.py
+++ remapped_hash/hash_table.py
@@ -6,8 +6,8 @@ class HashTable():
 	
 	TABLE_SIZE = 11
 	HASH_PRIMES = (19, 101, 17)
-	t1 = [[] for i in range(TABLE_SIZE)]
-	t2 = [[] for i in range(TABLE_SIZE)]
+	t1 = [None for _ in range(TABLE_SIZE)]
+	t2 = [None for _ in range(TABLE_SIZE)]
 
 	def __init__(self, size = None):
 		if size != None:
@@ -31,56 +31,72 @@ class HashTable():
 
 		return math.floor(self.TABLE_SIZE * (key * 0.9 - math.floor(key * 0.9)))
 
-	def add_value_with_key(self, key, value):
-		index = self._hash_by_string(key)
-		if index == None: 
-			print("Index Error")
-			return 0
-		
-		l = self._table[index]
-		l = [i for i in l if i[0] == key]
-		if len(l) > 0: 
-			return 1
-
-		self._table[index].append((key, value)[:])
-		return 1
-
-	def get_value_by_key(self, key):
-		index = self._hash_by_string(key)
-		if index == None: 
+	def search(self, key):
+		i = self.h1(key)
+		if i == None: 
 			print("Index Error")
 			return None
 
-		l = self._table[index]
-		l = [i for i in l if i[0] == key]
-
-		if len(l) > 0:
-			return l[0][1]
-		
-		return None
-
-	def find_value_by_key(self, key):
-		index = self._hash_by_string(key)
-		if index == None: 
-			print("Index Error")
+		if (self.t1[i] == None):
 			return None
 
-		l = self._table[index]
-		l = list(filter(lambda t: t[0] == key, l))
+		if (self.t1[i] == key):
+			return i
 
-		if len(l) > 0:
-			return 1
-		
-		return 0
+		return self.h2(key)
 
-	def remove_value_by_key(self, key):
-		index = self._hash_by_string(key)
-		if index == None: 
+	def insert(self, key):
+		i = self.h1(key)
+		if i == None: 
 			print("Index Error")
-			return 0
-
-		l = self._table[index]
-		l = list(filter(lambda t: t[0] != key, l))
-		self._table[index] = l
+			return
 		
-		return 1
+		if (self.t1[i] == None or self.t1[i] == -1):
+			self.t1[i] = key
+			return
+
+		if (self.t1[i] == key):
+			return
+		
+		k2 = self.t1[i]
+		j = self.h2(k2)
+		self.t2[j] = k2
+
+		self.t1[i] = key
+		return
+		
+	def remove(self, key):
+		j = self.h2(key)
+		if j == None: 
+			print("Index Error")
+			return
+		
+		if (self.t2[j] == key):
+			self.t2[j] = None
+			return
+
+		i = self.h1(key)
+		if i == None: 
+			print("Index Error")
+			return
+
+		self.t1[i] = -1
+		return
+
+	def print(self):
+		for i in range(self.TABLE_SIZE):
+			print("{} {}".format(self.t1[i], self.t2[i]))
+		print()
+
+	def line_print(self):
+		outs = []
+		for i in range(self.TABLE_SIZE):
+			if (self.t1[i] != None and self.t1[i] != -1):
+				outs.append(tuple((self.t1[i], "T1", i)))
+		for i in range(self.TABLE_SIZE):
+			if (self.t2[i] != None and self.t2[i] != -1):
+				outs.append(tuple((self.t2[i], "T2", i)))
+		
+		outs.sort(key = lambda t: t[0])
+		for l in outs:
+			print("{},{},{}".format(l[0], l[1], l[2]))
--- remapped_hash/my_hash.py
+++ remapped_hash/my_hash.py
@@ -1,9 +1,61 @@
 #!/usr/bin/env python3
 
-import hash_table
+"""
+Script de interface do usuario com uma tabela hash.
+Usa o modulo python 'hash_table.py',
+
+Comandos:
+    'i {x}'
+        Insere {x} na arvore AVL
+    'r {x}'
+        Remove {x} da arvore, se {x} pertence a mesma
+
+Apos cada comando, a tabela eh desenhada,
+"""
+
 from hash_table import HashTable
 
+table = HashTable()
+
+def process_input(line):
+    """
+    Processa linha de input.
+    Retorna 1 se a linha contem um comando valido (e o executa);
+    Retorna 0 caso contrario
+    """
+
+    try:
+        # separa a linha em comandos e argumentos
+        command, arguments = line.split(maxsplit=1)
+    except ValueError: # comando sem argumentos
+        command = line
+
+    # Comandos com argumentos:
+    try:
+        n = int(arguments)
+    except: # Argumento invalido
+        return 0
+
+    if (command == "i"): # input na árvore 
+        table.insert(n)
+    elif (command == "r"): # remove da árvore
+        table.remove(n)
+    else: # comando nao encontrado
+        return 0
+
+    return 1
+
+
 if __name__ == '__main__':
-	table = HashTable()
-	for i in range(12):
-		print(table.h1(i), table.h2(i))
+    try:
+        line = input()
+        while (line): # linha nao eh vazia
+            if (not process_input(line)):
+                print("Input inválido!")
+            # table.print() # descomente essa linha para ver a evolução da tabela
+            line = input()
+
+    except (EOFError, KeyboardInterrupt):
+        pass
+
+    table.line_print()
